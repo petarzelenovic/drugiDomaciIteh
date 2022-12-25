2c887367da425bf68e367cbf493b00cfa7d0ef99
Petar Zelenovic <petarzelenovic261@gmail.com>
2022-12-25 21:38:05 +0100

    izmena u migracijama

--- LaravelDomaci/database/migrations/2022_12_25_192034_create_products_table.php
+++ LaravelDomaci/database/migrations/2022_12_25_192034_create_products_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
 
             $table->text('name'); 
             $table->text('description')->nullable();   
-            $table->decimal('price', 10, 2)->nullable(); 
+            $table->decimal('price', 10, 2); 
             $table->text('slug');
 
             $table->foreignId('brand_id');
